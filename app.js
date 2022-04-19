fa80a4b6300f76991dc219b1b7cbc32d3b488df0
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2022-04-19 15:46:09 +0100

    added responsiveness

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const togglers = document.querySelectorAll(".menu-item-toggler");
 
 togglers.forEach((toggler) => {
   toggler.addEventListener("click", (e) => {
-    const menuItemWrapper = e.currentTarget.parentElement.parentElement.children[1];
+    const menuItemWrapper =
+      e.currentTarget.parentElement.parentElement.children[1];
     menuItemWrapper.classList.toggle("active");
-  })
+  });
+});
+const burger = document.querySelector(".fa-solid");
+const menu = document.querySelector(".nav-links");
+const navbar = document.querySelector(".navbar");
+
+burger.addEventListener("click", (e) => {
+  menu.classList.toggle("active");
+  burger.classList.toggle("fa-bars");
+  burger.classList.toggle("fa-xmark");
 });
 
+menu.addEventListener("click", (e) => {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+  }
+});
