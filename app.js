9b56839f971cb1e67724b1b39c7884cfe72b731c
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2022-04-23 22:05:49 +0100

    added strapi

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const togglers = document.querySelectorAll(".menu-item-toggler");
-
-togglers.forEach((toggler) => {
-  toggler.addEventListener("click", (e) => {
-    const menuItemWrapper =
-      e.currentTarget.parentElement.parentElement.children[1];
-    menuItemWrapper.classList.toggle("active");
-  });
-});
 const burger = document.querySelector(".fa-solid");
 const menu = document.querySelector(".nav-links");
 const navbar = document.querySelector(".navbar");
@@ -21,4 +12,52 @@ menu.addEventListener("click", (e) => {
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
   }
+});
+
+const url = "http://localhost:1337/api/menu-types?populate=menu";
+
+const fetchMenus = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  renderMenuTypes(data.data);
+};
+fetchMenus();
+
+const renderMenuTypes = (menuTypes) => {
+  menuTypes.forEach((menuType) => {
+    renderMenuCard(menuType);
+  });
+};
+
+const renderMenuNames = () => {
+  
+};
+
+const renderMenuCard = (menuType) => {
+  const menuTypesWrapper = document.querySelector(".menu-types-wrapper");
+  const { attributes } = menuType;
+  console.log(attributes.name);
+  const menuCard = `<div class="menu-type">
+  <h4 class="menu-type-title">${attributes.name}</h4>
+  <div class="line-separator"></div>
+  <div class="menu-item">
+  <div class="menu-item-title-container">
+  <h5 class="menu-item-title"></h5>
+  <p class="menu-item-toggler">+</p>
+  </div>
+  <ul class="menu-positions-wrapper">
+  </ul>
+  </div>
+  </div>`;
+  menuTypesWrapper.insertAdjacentHTML("beforeend", menuCard);
+};
+
+const togglers = document.querySelectorAll(".menu-item-toggler");
+
+togglers.forEach((toggler) => {
+  toggler.addEventListener("click", (e) => {
+    const menuItemWrapper =
+      e.currentTarget.parentElement.parentElement.children[1];
+    menuItemWrapper.classList.toggle("active");
+  });
 });
