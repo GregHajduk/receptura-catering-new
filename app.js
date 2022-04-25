201fe13e5431457f00f0905bb84acfcff805332e
GregHajduk <70976488+GregHajduk@users.noreply.github.com>
2022-04-25 17:53:50 +0100

    added strapi functionality

--- app.js
+++ app.js
@@ -20,6 +20,20 @@ const fetchMenus = async () => {
   const response = await fetch(url);
   const data = await response.json();
   renderMenuTypes(data.data);
+
+  const togglers = document.querySelectorAll(".menu-item-toggler");
+
+  if (togglers) {
+    togglers.forEach((toggler) => {
+      toggler.addEventListener("click", (e) => {
+        const menuItemWrapper =
+          e.currentTarget.parentElement.parentElement.children[1];
+        menuItemWrapper.classList.toggle("active");
+      });
+    });
+  }
+
+  console.log(togglers);
 };
 fetchMenus();
 
@@ -29,35 +43,38 @@ const renderMenuTypes = (menuTypes) => {
   });
 };
 
-const renderMenuNames = () => {
-  
-};
-
 const renderMenuCard = (menuType) => {
-  const menuTypesWrapper = document.querySelector(".menu-types-wrapper");
-  const { attributes } = menuType;
-  console.log(attributes.name);
-  const menuCard = `<div class="menu-type">
-  <h4 class="menu-type-title">${attributes.name}</h4>
-  <div class="line-separator"></div>
-  <div class="menu-item">
-  <div class="menu-item-title-container">
-  <h5 class="menu-item-title"></h5>
-  <p class="menu-item-toggler">+</p>
-  </div>
-  <ul class="menu-positions-wrapper">
-  </ul>
-  </div>
-  </div>`;
-  menuTypesWrapper.insertAdjacentHTML("beforeend", menuCard);
+  if (menuType) {
+    const menuTypesWrapper = document.querySelector(".menu-types-wrapper");
+    const { attributes } = menuType;
+    const menuCard = `<div class="menu-type">
+                        <h4 class="menu-type-title">${attributes.name}</h4>
+                        <div class="line-separator"></div>
+                        ${renderMenuContent(attributes.menu)}
+                      </div>`;
+    menuTypesWrapper.insertAdjacentHTML("beforeend", menuCard);
+  }
 };
 
-const togglers = document.querySelectorAll(".menu-item-toggler");
-
-togglers.forEach((toggler) => {
-  toggler.addEventListener("click", (e) => {
-    const menuItemWrapper =
-      e.currentTarget.parentElement.parentElement.children[1];
-    menuItemWrapper.classList.toggle("active");
-  });
-});
+const renderMenuContent = (menuContent) => {
+  let menuNames = "";
+  if (menuContent) {
+    menuContent.forEach((item) => {
+      const menuName = `
+      <div class="menu-item">
+        <div class="menu-item-title-container">
+          <h5 class="menu-item-title">${item.menuName}</h5>
+          <p class="menu-item-toggler">+</p>
+        </div>
+        <div class="menu-positions-wrapper">
+          <p class="menu-positions">${item.positions.replace(
+            /-/g,
+            "</br> -"
+          )}</p>
+        </div>
+      </div>`;
+      menuNames += menuName;
+    });
+  }
+  return menuNames;
+};
